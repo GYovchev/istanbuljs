09894badd042f1a56bdae154a5ec5d58b1e42846
Georgi Yovchev <gtyovchev@gmail.com>
2022-06-18 17:28:58 +0800

    asd

--- packages/istanbul-lib-instrument/src/visitor.js
+++ packages/istanbul-lib-instrument/src/visitor.js
@@ -410,12 +410,12 @@ class VisitState {
         const body = path.get('body');
         /* istanbul ignore else: not expected */
         if (body.isBlockStatement()) {
-            // body.node.body.unshift(T.variableDeclaration('let', [
-            //   T.variableDeclarator('__istanbuljs_coverage_state',
-            //     T.callExpression(
-            //       T.memberExpression(
-            //         T.identifier('JSON'), T.identifier('parse'),
-            //         [T.callExpression(T.identifier(this.varName), [])])))]));
+            body.node.body.unshift(T.variableDeclaration('let', [
+              T.variableDeclarator('__istanbuljs_coverage_state',
+                T.callExpression(
+                  T.memberExpression(
+                    T.identifier('JSON'), T.identifier('parse'),
+                    [T.callExpression(T.identifier(this.varName), [])])))]));
             body.node.body.unshift(T.expressionStatement(increment));
         } else {
             console.error(
